5cdd5f2d1580e568bb5f999438a83ae0398e43ca
zerai <teclaizerai@gmail.com>
2021-10-02 19:30:41 +0200

    Added AppHealtCheckController.

---
+++ src/Controller/AppHealtCheckController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppHealtCheckController extends AbstractController
+{
+    /**
+     * @Route("/sys/healt/check", name="app_healt_check")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'code' => 200,
+            'status' => 'active',
+        ]);
+    }
+}
